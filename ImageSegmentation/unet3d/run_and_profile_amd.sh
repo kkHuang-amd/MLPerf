6bb7ba2bebfa319c0096859f978b2ad0507fcacc
kangwangamd <100359556+kangwangamd@users.noreply.github.com>
2022-09-29 15:47:46 +0800

    Create run_and_profile_amd.sh
    add run_and_profile_amd.sh

---
+++ ImageSegmentation/unet3d/run_and_profile_amd.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+# runs benchmark and reports time to convergence
+# to use the script:
+#   run_and_time.sh <random seed 1-5>
+
+SEED=${1:--1}
+
+USER="amd"
+MAX_EPOCHS=1
+QUALITY_THRESHOLD="0.908"
+START_EVAL_AT=1000
+EVALUATE_EVERY=20
+LEARNING_RATE="2.0"
+LR_WARMUP_EPOCHS=1000
+# DATASET_DIR="$PWD/data"
+DATASET_DIR="/global/scratch/datasets/unet3d/kits19/data"
+PROCS=(8)
+BATCH_SIZE=2
+GRADIENT_ACCUMULATION_STEPS=1
+
+if [ -d ${DATASET_DIR} ]
+then
+  for NPROC in ${PROCS[@]}
+  do
+    # start timing
+    start=$(date +%s)
+    start_fmt=$(date +%Y-%m-%d\ %r)
+    echo "NPROC $NPROC, STARTING TIMING RUN AT $start_fmt"
+
+    # CLEAR YOUR CACHE HERE
+    python -c "
+from mlperf_logging.mllog import constants
+from runtime.logging import mllog_event
+mllog_event(key=constants.CACHE_CLEAR, value=True)"
+
+    torchrun --nproc_per_node=${NPROC} main.py \
+    --data_dir ${DATASET_DIR} \
+    --epochs ${MAX_EPOCHS} \
+    --evaluate_every ${EVALUATE_EVERY} \
+    --start_eval_at ${START_EVAL_AT} \
+    --quality_threshold ${QUALITY_THRESHOLD} \
+    --nproc_per_node ${NPROC} \
+    --platform ${USER} \
+    --batch_size ${BATCH_SIZE} \
+    --optimizer sgd \
+    --ga_steps ${GRADIENT_ACCUMULATION_STEPS} \
+    --learning_rate ${LEARNING_RATE} \
+    --seed ${SEED} \
+    --lr_warmup_epochs ${LR_WARMUP_EPOCHS}
+    # --cudnn_benchmark ## for NV only
+
+		# end timing
+    end=$(date +%s)
+    end_fmt=$(date +%Y-%m-%d\ %r)
+    echo "ENDING TIMING RUN AT $end_fmt"
+
+    # report result
+    result=$(( $end - $start ))
+    result_name="image_segmentation"
+    echo "NPROC $NPROC, RESULT,$result_name,$SEED,$result,$USER,$start_fmt"
+  done
+else
+        echo "Directory ${DATASET_DIR} does not exist"
+fi

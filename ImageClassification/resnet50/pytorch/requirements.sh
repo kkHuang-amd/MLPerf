0874cf755fa2f2e385d144a5f83d304e2cd8e964
luise1030 <chien-hao.chen@amd.com>
2022-09-30 06:17:23 +0200

    Add --user for pip bare-metal installation

--- ImageClassification/resnet50/pytorch/requirements.sh
+++ ImageClassification/resnet50/pytorch/requirements.sh
@@ -1,8 +1,9 @@
 ### Simple script to install dependencies for ResNet50
 apt-get update -y && apt-get install -y numactl && apt-get install hwloc
 
-python3 -m pip install --no-cache-dir git+https://github.com/mlcommons/logging.git@2.0.0-rc4
-python3 -m pip install deepspeed
+export MAX_JOBS=128
+python3 -m pip install --no-cache-dir git+https://github.com/mlcommons/logging.git@2.0.0-rc4 --user
+python3 -m pip install deepspeed --user
 HOROVOD_WITHOUT_MPI=1 HOROVOD_GPU_OPERATIONS=NCCL HOROVOD_GPU=ROCM python3 -m pip install --user -v git+https://github.com/horovod/horovod.git@9d56c5a6d55e9619e100d19480a11952d116f4b3
 python3 -m pip install -v --install-option="--cpp_ext" --install-option="--cuda_ext" 'git+https://github.com/ROCmSoftwarePlatform/apex.git' --user
 
@@ -10,5 +11,5 @@ pushd src/fused_lars
 bash build.sh
 popd
 pushd src/utils
-python3 setup.py install
+python3 setup.py install --user
 popd

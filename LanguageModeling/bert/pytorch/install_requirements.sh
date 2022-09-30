506986bad4910aea71db20eb325dbfc04debb8c7
wunhuang <wunhuang@amd.com>
2022-09-30 06:04:57 +0000

    [Bert] update install script to replace distributed_fused_lamb implementation

--- LanguageModeling/bert/pytorch/install_requirements.sh
+++ LanguageModeling/bert/pytorch/install_requirements.sh
@@ -4,6 +4,10 @@ pip3 install --no-cache-dir -r requirements.txt
 cd mhalib
 python3 setup.py build && cp build/lib*/mhalib* ../
 cd ../
+
+apex_path=`pip show apex | grep Location | awk -F ":" '{print $2}'`
+
+cp optim/distributed_fused_lamb.py ${apex_path}/apex/contrib/optimizers/
 #cd apexcontrib
 #python setup.py install --distributed_lamb --deprecated_fused_adam
 #cd ../
